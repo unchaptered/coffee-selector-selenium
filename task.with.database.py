d54717d790e747ea1fe36afaecb366e70407f829
Unchaptered <workstation19961002@gmail.com>
2022-07-13 17:12:21 +0900

    Modify `task.with.database.py`

--- task.with.database.py
+++ task.with.database.py
@@ -20,17 +20,21 @@ card_group = driver.find_elements(By.CSS_SELECTOR, '#coffee-selector > div > div
 
 capsule_list = []
 for card_vertical in card_group:
+
     if card_vertical is not None:
+
         card_horizon = card_vertical.find_elements(By.CSS_SELECTOR, 'li')
+
         for card in card_horizon:
+
             if card is not None:
+
                 capsule_title = card.find_element(By.CSS_SELECTOR, 'h4')
                 capsule_strong = card.find_element(By.CSS_SELECTOR, 'span')
+
                 if capsule_title is not None and capsule_strong is not None:
+
                     print(capsule_title.text, capsule_strong.text)
-                    # is_valid = database[COLLECTION_CAPSULE].find_one({
-                    #     'capsule_title': capsule_title.text
-                    # })
                     capsule_list.append({
                         'capsule_title': capsule_title.text,
                         'capsule_strong': int(capsule_strong.text)
