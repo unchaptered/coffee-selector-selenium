8153c3f57552293af51ea8f714be6536e947289e
Unchaptered <workstation19961002@gmail.com>
2022-07-13 17:07:11 +0900

    Add `task.with.database.py`

---
+++ task.with.database.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+from modules.env import DRIVER_PATH, MONGO_URL, DATABASE_NAME, COLLECTION_CAPSULE
+from modules.database import getMongoClient
+
+database = getMongoClient(MONGO_URL)[DATABASE_NAME]
+
+options = webdriver.ChromeOptions()
+options.add_argument('window-size=1920,1080')
+
+driver = webdriver.Chrome(DRIVER_PATH, options=options)
+
+driver.implicitly_wait(10)
+driver.get('https://www.nespresso.com/kr/ko/grands-crus-coffee-range#!/by-range')
+
+card_group = driver.find_elements(By.CSS_SELECTOR, '#coffee-selector > div > div > div > ul > li')
+
+# try:
+
+capsule_list = []
+for card_vertical in card_group:
+    if card_vertical is not None:
+        card_horizon = card_vertical.find_elements(By.CSS_SELECTOR, 'li')
+        for card in card_horizon:
+            if card is not None:
+                capsule_title = card.find_element(By.CSS_SELECTOR, 'h4')
+                capsule_strong = card.find_element(By.CSS_SELECTOR, 'span')
+                if capsule_title is not None and capsule_strong is not None:
+                    print(capsule_title.text, capsule_strong.text)
+                    # is_valid = database[COLLECTION_CAPSULE].find_one({
+                    #     'capsule_title': capsule_title.text
+                    # })
+                    capsule_list.append({
+                        'capsule_title': capsule_title.text,
+                        'capsule_strong': int(capsule_strong.text)
+                    })
+
+database[COLLECTION_CAPSULE].insert_many(capsule_list)
+
+# except:
+#     print('에러')
